05036d84bf927a2a200527032257ba7524ca396b
Thomas Coats <github@voodoolabs.net>
2015-12-09 14:34:09 +1300

    Fixed

--- index.coffee
+++ index.coffee
@@ -2,6 +2,6 @@ module.exports = (lng, lat, zoom) ->
   n = Math.pow(2, zoom)
   [
     Math.floor(n * ((lng + 180) / 360))
-    Math.floor(n * (1 - (Math.log(Math.tan(lat) + 1 / Math.cos(lat)) / Math.PI)) / 2)
+    Math.floor(n * (1.0 - Math.log(Math.tan(lat * Math.PI / 180.0) + 1.0 / Math.cos(lat * Math.PI / 180.0)) / Math.PI) / 2.0)
     zoom
   ]
--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 module.exports = function(lng, lat, zoom) {
   var n;
   n = Math.pow(2, zoom);
-  return [Math.floor(n * ((lng + 180) / 360)), Math.floor(n * (1 - (Math.log(Math.tan(lat) + 1 / Math.cos(lat)) / Math.PI)) / 2), zoom];
+  return [Math.floor(n * ((lng + 180) / 360)), Math.floor(n * (1.0 - Math.log(Math.tan(lat * Math.PI / 180.0) + 1.0 / Math.cos(lat * Math.PI / 180.0)) / Math.PI) / 2.0), zoom];
 };
